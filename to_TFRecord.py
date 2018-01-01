c9e9e5873ad493f0773463bb39c7ee6b8b6d334c
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-01-02 02:55:18 +0900

    Add progres bar, 外部モジュール依存無し

--- to_TFRecord.py
+++ to_TFRecord.py
@@ -2,23 +2,22 @@
 
 # Inside modules
 import os
+import sys
 # Outside modules
 import tensorflow as tf
 import numpy as np
-import click
 from PIL import Image # pip install Pillow
 
-@click.command()
-@click.argument('filedir')
-def main(filedir):
+
+def main(file_path):
 	dataset_path = "./dataset.tfrecord"
 	width, height = 96, 96
 	channels = 3
-	datas = os.listdir(filedir)
+	datas = os.listdir(file_path)
 	writer = tf.python_io.TFRecordWriter(dataset_path)
-	for img_name in datas:
+	for i, img_name in enumerate(datas):
 		# Resize
-		img_obj = Image.open(filedir + "/" + img_name).convert("RGB").resize((width, height))
+		img_obj = Image.open(file_path + "/" + img_name).convert("RGB").resize((width, height))
 		# Convert to bytes
 		img = np.array(img_obj).tostring()
 
@@ -33,7 +32,34 @@ def main(filedir):
 						int64_list=tf.train.Int64List(value=[channels])),
 		}))
 		writer.write(record.SerializeToString())
+
+		bar, percent = calc_bar(i+1, len(datas))
+		sys.stdout.write("\r{}/{} [{}] - {}%".format(i+1, len(datas), bar, percent))
 	writer.close()
+	print("\ndone.")
+
+def calc_bar(now_count, max_count):
+	max_bar_size = 50
+	percent = (now_count*100) // max_count
+	bar_num = percent // 2
+	bar = ""
+	if (bar_num - 1) > 0:
+		for _ in range(bar_num - 1):
+			bar += "="
+		bar += ">"
+		for _ in range(max_bar_size - bar_num):
+			bar += " "
+	elif bar_num == 1:
+		bar = ">"
+		for _ in range(max_bar_size - 1):
+			bar += " "
+	else:
+		for _ in range(max_bar_size):
+			bar += " "
+	return bar, percent
 
 if __name__ == '__main__':
-	main()
+	if len(sys.argv) != 2:
+		print(':Usage: python %s file_path' %sys.argv[0])
+	else:
+		main(sys.argv[1])
